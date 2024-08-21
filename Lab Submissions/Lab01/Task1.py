e3f641a7f48d9371d15cf41792ba2b83c160eb8c
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-08-21 10:09:58 +0500

    Create Task1.py

---
+++ Lab Submissions/Lab01/Task1.py
@@ -0,0 +1,11 @@
+# Online Python compiler (interpreter) to run Python online.
+# TASK 1
+print("BMI INDEX")
+weight = float(input("ENTER YOUR WEIGHT: "))
+height = float(input("ENTER YOUR HEIGHT: "))
+
+print (BMI := weight/(height)**2)
+
+
+#alternative
+print ("BMI: ",  weight/(height)**2)
