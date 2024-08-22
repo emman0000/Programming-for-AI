46f6d8de5ce7d6109e4091d0798bcd4bfba54e83
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-08-22 23:21:52 +0500

    Create Task7.py

---
+++ Lab Submissions/Lab01/Task7.py
@@ -0,0 +1,8 @@
+word = input("Enter a word: ")
+# Flip the word using a loop
+
+flipped_word = ""
+
+for Word in word:
+    flipped_word = Word + flipped_word #concatenation here for reversing the word
+print (flipped_word)
