6deb133a63be1a087fb8fa2f0605650a4648686d
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-08-23 19:03:36 +0500

    Update Task12.py

--- Lab Submissions/Lab01/Task12.py
+++ Lab Submissions/Lab01/Task12.py
@@ -1,1 +1,11 @@
 
+dictionary ={}
+
+for num in range(1,16): #I have to start at 1 and end at 16 to include 15
+  dictionary[num] = num**2
+
+
+
+print("Number : Squared")
+for key, value in dictionary.items():
+   print(f"{key} : {value}" )
