188e8906867c32bd5be3db7b2349909c55370bc5
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-08-27 21:44:53 +0500

    Update Task1.py

--- Lab Submissions/Lab02/Task1.py
+++ Lab Submissions/Lab02/Task1.py
@@ -1,1 +1,11 @@
 
+#task 1 calculate area 
+
+def area(length, width):
+    return length * width
+
+length = int(input("Enter the length of the rectangle: "))
+width = int(input("Enter the width of the rectangle: "))
+
+my_area = area(length, width)
+print("Area: ", my_area)
