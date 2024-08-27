d55c8dfd33af14e7051817293edf7b9e812a65f1
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-08-27 21:58:10 +0500

    Update Task2.py

--- Lab Submissions/Lab02/Task2.py
+++ Lab Submissions/Lab02/Task2.py
@@ -1,1 +1,16 @@
+def search(input_string):
+    last_letter = input_string[-1]
+    
+    # Check if the last character is an alphabet
+    if last_letter.isalpha():
+        if last_letter in "aeiouAEIOU":
+            return "Vowel"
+        else:
+            return "Consonant"
+   
+
+# Get input from the user
+input_string = input("Enter a string: ")
+result = search(input_string)
+print(result)
 
