7db9046bba1590c70843afba2e5760c0a63efb39
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-08-27 22:05:56 +0500

    Update Task3.py

--- Lab Submissions/Lab02/Task3.py
+++ Lab Submissions/Lab02/Task3.py
@@ -1,1 +1,15 @@
+def employee(name, salary):
+    if salary == 0:  # Correct condition to check if salary is 0
+        salary = 10000
+    tax = salary * 0.02
+    salary_after_tax = salary - tax
+    print(f"Employee Name: {name}")
+    print(f"Salary after tax: {salary_after_tax}")
+
+# Get inputs from the user
+n = input("Enter the name of the employee: ")
+s = float(input("Enter the salary of the employee: "))
+
+# Call the function
+employee(n, s)
 
