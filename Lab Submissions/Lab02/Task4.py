94309310f9b3ac0da42743eadd77021b1be368b8
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-08-28 10:02:36 +0500

    Update Task4.py

--- Lab Submissions/Lab02/Task4.py
+++ Lab Submissions/Lab02/Task4.py
@@ -1,10 +1,16 @@
-#factorial of n
 
-def factorial(n):
-  if n==0:
-    return 1
-  else:
-    return n*factorial(n-1)#recursion 
+#task 4 
+def employee(name, salary):
+    if salary == 0:  # Correct condition to check if salary is 0
+        salary = 10000
+    tax = salary * 0.02
+    salary_after_tax = salary - tax
+    print(f"Employee Name: {name}")
+    print(f"Salary after tax: {salary_after_tax}")
 
-n = int(input("Enter a number: "))
-print(factorial(n))
+# Get inputs from the user
+n = input("Enter the name of the employee: ")
+s = float(input("Enter the salary of the employee: "))
+
+# Call the function
+employee(n, s)
