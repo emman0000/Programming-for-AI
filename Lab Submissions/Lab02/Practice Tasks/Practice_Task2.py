2f313a2a6349209dc8bdc4e19f019322d4fd5b25
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-08-28 09:41:20 +0500

    Update Practice_Task2.py

--- Lab Submissions/Lab02/Practice Tasks/Practice_Task2.py
+++ Lab Submissions/Lab02/Practice Tasks/Practice_Task2.py
@@ -1,1 +1,11 @@
+def show_info(**details):
+    result = ""
+    for key, value in details.items():
+        result += f"{key.capitalize()}: {value}\n"
+    
+    return result
+
+
+info = show_info(name="Emman", age=69, city="Karachi", occupation="Professional Sleeper")
+print(info)
 
