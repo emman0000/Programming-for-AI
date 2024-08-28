f083d061883352ec538370a4be0ef605dbee1dc8
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-08-28 12:03:07 +0500

    Update Task7.py

--- Lab Submissions/Lab02/Task7.py
+++ Lab Submissions/Lab02/Task7.py
@@ -1,16 +1,20 @@
+#task 7
 def average(temp_list):
-    total_sum = sum(temp_list) 
-   
-    avg_temp = ( total_sum / len(temp_list))
-     
-     print("Average temperature over the span of 30 days is:", avg_temp)
-  
-    sorted_temps = sorted(temp_list) print("Sorted temperatures:", sorted_temps)
-    max_temp = max(temp_list) print("Maximum Temperature:", max_temp) 
-    min_temp = min(temp_list) print("Minimum Temperature:", min_temp) 
-    
-    # List of temperatures over 30 days
-    temp_list = [12, 15, 33, 45, 22, 12, 15, 15, 17, 20, 12, 15, 33, 45, 22, 12, 15, 15, 17, 20, 12, 15, 33, 45, 22, 12, 15, 15, 17, 20] 
+  total_sum = sum(temp_list) 
 
+  avg_temp = ( total_sum / len(temp_list))
 
-    average(temp_list)  
+  print("Average temperature over the span of 30 days is:", avg_temp)
+
+  sorted_temps = sorted(temp_list) 
+  print("Sorted temperatures:", sorted_temps)
+  max_temp = max(temp_list) 
+  print("Maximum Temperature:", max_temp) 
+  min_temp = min(temp_list)
+  print("Minimum Temperature:", min_temp) 
+
+  # List of temperatures over 30 days
+
+temp_list = [12, 15, 33, 45, 22, 12, 15, 15, 17, 20, 12, 15, 33, 45, 22, 12, 15, 15, 17, 20, 12, 15, 33, 45, 22, 12, 15, 15, 17, 20] 
+
+average(temp_list)
