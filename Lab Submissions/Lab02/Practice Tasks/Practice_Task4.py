882300d396f0fffc3cd71d0ee07c15a6ef2d1068
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-08-28 09:46:38 +0500

    Update Practice_Task4.py

--- Lab Submissions/Lab02/Practice Tasks/Practice_Task4.py
+++ Lab Submissions/Lab02/Practice Tasks/Practice_Task4.py
@@ -1,18 +1,1 @@
-#Practice Task 2 
-def interact_with_user():
-    user_name = input("What is your good name? ")
-    print(f"Hello, {user_name.title()}!")
-
-    print("I hope you are fine. Let me know how I can help you! (Start asking)")
-    response = input("Are you doing well? (yes/no): ").strip().lower()
-
-    if response == "yes":
-        problem = input("Please share your problem with us: ")
-        print("Thanks for your feedback, we will resolve your problem.")
-    else:
-       
-        goodbye_message = "Okay! Good to see you, stay connected"
-        print(goodbye_message.center(80))
-      
-interact_with_user()
 
