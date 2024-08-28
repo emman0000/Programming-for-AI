852c4babeedb59be9e3834e65587fb59e8b04b3b
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-08-28 10:05:27 +0500

    Update Task5.py

--- Lab Submissions/Lab02/Task5.py
+++ Lab Submissions/Lab02/Task5.py
@@ -1,21 +1,2 @@
-def multiply(numbers):
-    # Check if there's a 0 in the list, if so return 0
-    if 0 in numbers:
-        return 0
 
-    # Initialize the product to 1
-    product = 1
-
-    # Multiply each number in the list
-    for num in numbers:
-        product *= num
-    
-    return product
-
-# Get input from the user and convert it to a list of numbers
-input_list = input("Enter a list of numbers separated by spaces: ").split()
-input_list = [float(num) for num in input_list]  # Convert each input to a float
-
-# Call the function and print the result
-print(multiply(input_list))
 
