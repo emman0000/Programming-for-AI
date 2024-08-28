933ee7f1d9df938ab32623716ea94c901318982d
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-08-28 10:11:29 +0500

    Create Task7.py

---
+++ Lab Submissions/Lab02/Task7.py
@@ -0,0 +1,5 @@
+#7. You have a list of daily temperatures recorded over a month. Write code to:
+# Calculate and print the average temperature for the month.
+# Find and print the highest and lowest temperatures.
+# Sort the temperatures in ascending order.
+# Remove the temperature record for a specific day
