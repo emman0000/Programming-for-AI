49519f2e3de7a1e25fd97c9bc3f304d630ab455a
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-09-11 09:45:40 +0500

    Update task3.py

--- Lab Submissions/Lab04/task3.py
+++ Lab Submissions/Lab04/task3.py
@@ -1,1 +1,20 @@
 
+"""
+Make a class "Student"and make a function "Print_biodata" inside it. Pass name and age of
+student to constructor. Now access "Print_biodata" function using object to print name and age of student.
+
+"""
+
+class Student :
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def Print_Biodata(self):
+         print("Name of Person: {} ".format(self.name))
+         print("Age of Person: {} ".format(self.age))
+
+
+s1 = Student("Emman " , 23)
+s1.Print_Biodata()
