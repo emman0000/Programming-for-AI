89c0fa310606cb936e54efe559578e9b429fd533
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-09-15 18:38:12 +0500

    Update task9.py

--- Lab Submissions/Lab04/task9.py
+++ Lab Submissions/Lab04/task9.py
@@ -1,1 +1,41 @@
 
+
+class Student:
+    def __init__(self, student_id, name):
+        self.student_id = student_id
+        self.name = name
+
+    def display_student_info(self):
+        print(f"Student ID: {self.student_id}")
+        print(f"Name: {self.name}")
+
+
+class Marks(Student):
+    def __init__(self, student_id, name, marks_algo, marks_dataScience, marks_calculus):
+        super().__init__(student_id, name)
+        self.marks_algo = marks_algo
+        self.marks_dataScience = marks_dataScience
+        self.marks_calculus = marks_calculus
+
+    def display_marks(self):
+      
+        print(f"Marks in Algorithm: {self.marks_algo}")
+        print(f"Marks in Data Science: {self.marks_dataScience}")
+        print(f"Marks in Calculus: {self.marks_calculus}")
+
+class Result(Marks):
+    def __init__(self, student_id, name, marks_algo, marks_dataScience, marks_calculus):
+        super().__init__(student_id, name, marks_algo, marks_dataScience, marks_calculus)
+
+    def calculate_result(self):
+     
+        total_marks = self.marks_algo + self.marks_dataScience + self.marks_calculus
+        average_marks = total_marks / 3
+        print(f"Total Marks: {total_marks}")
+        print(f"Average Marks: {average_marks}")
+
+
+student_result = Result(100, "Emman Ali", 82 ,90, 64)
+student_result.display_student_info()
+student_result.display_marks()
+student_result.calculate_result()
