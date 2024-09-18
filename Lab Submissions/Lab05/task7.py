f9a0f82152458a566776de9339fd6404767b1ab2
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-09-18 09:41:28 +0500

    Update task7.py

--- Lab Submissions/Lab05/task7.py
+++ Lab Submissions/Lab05/task7.py
@@ -1,1 +1,17 @@
 
+import re
+
+def extract_emails(text):
+    pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    emails = re.findall(pattern, text)
+    return emails
+
+text = """
+My personal email address is areeba.hasnain@gmail.com
+My FAST email address is k230059@nu.edu.pk
+"""
+
+emails = extract_emails(text)
+print("Extracted email addresses:")
+for email in emails:
+    print(email)
