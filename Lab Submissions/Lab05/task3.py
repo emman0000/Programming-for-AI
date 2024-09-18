547908893882f3892c3640e95c3e7cc5e6811cf0
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-09-18 09:22:39 +0500

    Update task3.py

--- Lab Submissions/Lab05/task3.py
+++ Lab Submissions/Lab05/task3.py
@@ -1,1 +1,20 @@
+"""
+Write a program in which a class named Account has private member variables named
+account_no ,account_bal ,security_code. Use a public function to initialize the variables and
+print all data.
+"""
 
+class Account:
+    def __init__(self, account_no, account_bal, security_code):
+        self.account_no = account_no
+        self.account_bal = account_bal
+        self.security_code = security_code
+    def print(self):
+        print("Account Number:", self.account_no)
+        print("Account Balance:", self.account_bal)
+        print("Security Code:", self.security_code)
+
+
+# Example usage
+account = Account("123456789", 1000.0, "123")
+account.print()
