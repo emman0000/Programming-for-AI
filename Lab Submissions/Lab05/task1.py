f942f02d2a62e453f531bff576fe1d76206d75da
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-09-18 08:56:49 +0500

    Update task1.py

--- Lab Submissions/Lab05/task1.py
+++ Lab Submissions/Lab05/task1.py
@@ -1,1 +1,17 @@
+lass Vehicle:
+    def __init__(self, seating_capacity):
+        self.seating_capacity = seating_capacity
 
+    def calculate_fare(self):
+        return self.seating_capacity * 100
+
+
+class Bus(Vehicle):
+    def calculate_fare(self):
+        base_fare = super().calculate_fare()
+        maintenance_charge = base_fare * 0.10
+        return base_fare + maintenance_charge
+
+
+bus2 = Bus(75)  
+print(bus2.calculate_fare())
