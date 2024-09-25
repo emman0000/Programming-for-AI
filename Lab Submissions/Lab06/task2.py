8da26ccec4cc5dfd7e91a71f9b9ac8bf3988d727
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-09-25 10:03:28 +0500

    Update task2.py

--- Lab Submissions/Lab06/task2.py
+++ Lab Submissions/Lab06/task2.py
@@ -1,1 +1,12 @@
 
+import pandas as pd
+
+movie_data = {
+    'Title': ['Tangled', 'Mr and Mrs Smith', 'Legally Blonde', 'Iron Man', 'Madagascar 4'],
+    'Runtime': [152, 163, 169, 420, 154], 
+    'Year': [2010, 2019, 2014, 2008, 1994]
+}
+
+df = pd.DataFrame(movie_data)
+df_sorted = df.sort_values(by='Runtime', ascending=False)
+print(df_sorted)
