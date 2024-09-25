d350d3646e0f18a933ad94e21fa4c2b2f17858bc
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-09-25 10:04:55 +0500

    Update task4.py

--- Lab Submissions/Lab06/task4.py
+++ Lab Submissions/Lab06/task4.py
@@ -1,1 +1,14 @@
+import pandas as pd
 
+data = {'A': [1, 2, 3, 4, 5], 
+        'B': [6, 7, 8, 9, 10]}
+df = pd.DataFrame(data)
+
+print("Original DataFrame:")
+print(df)
+
+df.reset_index(drop=True, inplace=True)
+df.index += 1
+
+print("\nModified DataFrame:")
+print(df)
