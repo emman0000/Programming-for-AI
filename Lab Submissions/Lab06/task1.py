786dc3398b5e88c09acad87243e05db7768aeca4
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-09-25 10:05:58 +0500

    Update task1.py

--- Lab Submissions/Lab06/task1.py
+++ Lab Submissions/Lab06/task1.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+movie_data = {
+    'Title': ['Movie 1', 'Movie 2', 'Movie 3', 'Movie 4', 'Movie 5'],
+    'Revenue (in million)': [3.5, 2.1, 1.8, 5.0, 0.9], 
+    'Budget (in million)': [0.8, 1.5, 0.5, 0.7, 0.6]  
+}
+
+df = pd.DataFrame(movie_data)
+filtered_movies = df[(df['Revenue (in million)'] > 2) & (df['Budget (in million)'] < 1)]
+print(filtered_movies)
