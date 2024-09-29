d26c3d109b89d8bee8f9f2fe9dd16bad79d580fe
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-09-29 19:37:21 +0500

    Update task7.py

--- Lab Submissions/Lab06/task7.py
+++ Lab Submissions/Lab06/task7.py
@@ -1,1 +1,10 @@
 
+import pandas as pd
+#question did not specify a csv file so generic csv file name has been used 
+df = pd.read_excel(r"C:\Users\Pc01\Documents\employee.xlsx")
+print(df.head) #to check the right file is being used
+
+specified_year = 1983
+
+employees_of_the_year = df[df['Year']==specified_year]
+print(employees_of_the_year)
