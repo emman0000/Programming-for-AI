2d2f38374c2b12688b7f5b7b42ec15825689de03
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-09-29 07:47:37 +0500

    Update task6.py

--- Lab Submissions/Lab05/task6.py
+++ Lab Submissions/Lab05/task6.py
@@ -1,42 +1,54 @@
+"""
+# 1. Create a base class "Employee" with attributes: "name" and "salary".
+# 2. Add a method "calculateBonus" in Employee to calculate bonus based on salary.
+#    - Managers get a 20% bonus.
+#    - Developers get a 10% bonus.
+# 3. Create two subclasses: "Manager" and "Developer" inheriting from "Employee".
+#    - "Manager" has a method "hire" that logs a message when hiring someone.
+#    - "Developer" has a method "writeCode" that logs a message when writing code.
+# 4. Create a subclass "SeniorManager" inheriting from "Manager".
+#    - Override the "calculateBonus" method to give senior managers a 30% bonus.
 
+"""
+
+
+
+#base class
 class Employee:
-    def __init__(self, name, salary):
+    def __init__(self,name,salary):
         self.name = name
         self.salary = salary
 
-    def calculate_bonus(self):
-        pass
-
+    def calc_bonus(self):
+        return 0
 
 class Manager(Employee):
-    def calculate_bonus(self):
-        return self.salary * 0.20
+    def calc_bonus(self):
+        return self.salary*0.2
 
     def hire(self):
         print(f"{self.name} is hiring someone.")
 
-
 class Developer(Employee):
-    def calculate_bonus(self):
-        return self.salary * 0.10
+    def calc_bonus(self):
+        return self.salary*0.1
 
-    def write_code(self):
-        print(f"{self.name} is writing code.")
-
-
-class SeniorManager(Manager):
-    def calculate_bonus(self):
-        return self.salary * 0.30
+    def writecode(self):
+        print(f"{self.name} is writing code")
+class senior_Manager(Manager):
+    def calc_bonus(self):
+        return self.salary*0.3
 
 
-manager = Manager("Areeba", 100000)
-developer = Developer("Emman", 80000)
-senior_manager = SeniorManager("Taha", 150000)
-
-print(f"{manager.name}'s bonus: Rs {manager.calculate_bonus():.2f}")
-print(f"{developer.name}'s bonus: Rs {developer.calculate_bonus():.2f}")
-print(f"{senior_manager.name}'s bonus: Rs {senior_manager.calculate_bonus():.2f}")
-
+manager = Manager("Hugh Jackman", 77000)
+print(f"Manager Bonus: {manager.calc_bonus()}")
 manager.hire()
-developer.write_code()
-senior_manager.hire()
+
+developer = Developer("Henry Cavill", 42000)
+print(f"Developer Bonus: {developer.calc_bonus()}")
+developer.writecode()
+
+senior_Manager = senior_Manager("Pierce Brosnan", 72000)
+print(f"Senior Manager Bonus: {senior_Manager.calc_bonus()}")
+
+#end
