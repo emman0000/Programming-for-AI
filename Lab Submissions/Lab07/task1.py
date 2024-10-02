d1301bb1b6a4e9f8d61fba916b6ec2a83419d779
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-10-02 10:24:45 +0500

    Update task1.py

--- Lab Submissions/Lab07/task1.py
+++ Lab Submissions/Lab07/task1.py
@@ -1,1 +1,9 @@
+import pandas as pd
+#use this to show all the data
+pd.set_option('display.max_rows', None)
+df = pd.read_csv(r"C:\Users\Pc01\Documents\heart.csv")
+df_filter = df[df['ca']==2]
+
+print(df_filter)
+
 
