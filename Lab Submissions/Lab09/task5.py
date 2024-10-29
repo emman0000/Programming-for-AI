78041f391b3efbbed685725399b7d85a5e61c553
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-10-29 21:29:54 +0500

    Update task5.py

--- Lab Submissions/Lab09/task5.py
+++ Lab Submissions/Lab09/task5.py
@@ -1,1 +1,11 @@
+import matplotlib.pyplot as plt
+
+genders = ['Male', 'Female','Other'] #it's 2024
+gender_distribution = [69, 30,1]
+
+plt.figure(figsize=(8, 6))
+plt.pie(gender_distribution, labels=genders, autopct='%1.1f%%', startangle=140)
+plt.title("Gender Distribution Among Students", fontsize=16)
+plt.axis('equal')
+plt.show()
 
