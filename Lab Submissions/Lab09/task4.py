4085ce198f772268f7bdbc2e94e330a35598363d
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2024-10-29 21:30:20 +0500

    Update task4.py

--- Lab Submissions/Lab09/task4.py
+++ Lab Submissions/Lab09/task4.py
@@ -1,1 +1,12 @@
 
+import matplotlib.pyplot as plt
+
+age_groups = ['18-25', '26-30', '31-35', '36 and above']
+age_distribution = [69, 70, 42, 7]
+
+
+plt.figure(figsize=(8, 6))
+plt.pie(age_distribution, labels=age_groups, autopct='%1.1f%%', startangle=140)
+plt.title("Distribution of Student Ages", fontsize=16)
+plt.axis('equal')
+plt.show()
